23c24b5f2ea6841bbe2fdd6697a202086bad58c2
Tim Golden <mail@timgolden.me.uk>
2015-10-15 11:28:56 +0100

    * We're only supporting 2.7 & 3.3+ now

--- make_all.bat
+++ make_all.bat
@@ -15,9 +15,6 @@ rem Yuck - 2to3 hackery - must nuke bdist dirs as it may hold py3x syntax.
 if exist build/bdist.win32/. rm -rf build/bdist.win32
 if exist build/bdist.win-amd64/. rm -rf build/bdist.win-amd64
 py -2.7-32 setup.py -q sdist
-py -2.6-32 setup.py -q bdist_wininst --target-version=2.5 --skip-build
-py -2.6-32 setup.py -q bdist_wininst --target-version=2.6 --skip-build
-py -2.6-32 setup.py -q bdist_wininst --target-version=2.6 --skip-build --plat-name=win-amd64 
 py -2.7-32 setup.py -q bdist_wininst --target-version=2.7 --skip-build
 py -2.7-32 setup.py -q bdist_wininst --target-version=2.7 --skip-build --plat-name=win-amd64 
 
@@ -25,20 +22,14 @@ rem Just incase - re-nuke bdist dirs so 2to3 always runs.
 if exist build/bdist.win32/. rm -rf build/bdist.win32
 if exist build/bdist.win-amd64/. rm -rf build/bdist.win-amd64
 
-py -3.1-32 setup3.py -q bdist_wininst --target-version=3.1 --skip-build
-py -3.1-32 setup3.py -q bdist_wininst --target-version=3.1 --skip-build --plat-name=win-amd64 
+py -3.3-32 setup.py -q bdist_wininst --target-version=3.3 --skip-build
+py -3.3-32 setup.py -q bdist_wininst --target-version=3.3 --skip-build --plat-name=win-amd64 
 
-py -3.2-32 setup3.py -q bdist_wininst --target-version=3.2 --skip-build
-py -3.2-32 setup3.py -q bdist_wininst --target-version=3.2 --skip-build --plat-name=win-amd64 
+py -3.4-32 setup.py -q bdist_wininst --target-version=3.4 --skip-build
+py -3.4-32 setup.py -q bdist_wininst --target-version=3.4 --skip-build --plat-name=win-amd64 
 
-py -3.3-32 setup3.py -q bdist_wininst --target-version=3.3 --skip-build
-py -3.3-32 setup3.py -q bdist_wininst --target-version=3.3 --skip-build --plat-name=win-amd64 
-
-py -3.4-32 setup3.py -q bdist_wininst --target-version=3.4 --skip-build
-py -3.4-32 setup3.py -q bdist_wininst --target-version=3.4 --skip-build --plat-name=win-amd64 
-
-py -3.5-32 setup3.py -q bdist_wininst --target-version=3.5 --skip-build
-py -3.5-32 setup3.py -q bdist_wininst --target-version=3.5 --skip-build --plat-name=win-amd64 
+py -3.5-32 setup.py -q bdist_wininst --target-version=3.5 --skip-build
+py -3.5-32 setup.py -q bdist_wininst --target-version=3.5 --skip-build --plat-name=win-amd64 
 
 rem And nuke the dirs one more time :)
 if exist build/bdist.win32/. rm -rf build/bdist.win32
